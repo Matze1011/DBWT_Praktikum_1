044de3e155ee20052cb17c15c3b946825d2bb4ca
Matze1011 <58034109+Matze1011@users.noreply.github.com>
2020-11-05 22:47:35 +0100

    Meilenstein 2: Aufgabe 5 komplett fertig! Fehlen 6 und 7.

---
+++ beispiele/m2_4d_array.php
@@ -0,0 +1,59 @@
+<?php
+/**
+-Praktikum DBWT. Autoren:
+-Marcel, Buschmann, 3220186
+-Michael Buschmann, 3220197
+ */
+
+$famousMeals = [
+    1 => ['name' => 'Currywurst mit Pommes',
+        'winner' => [2001, 2003, 2007, 2010, 2020]],
+    2 => ['name' => 'Hähnchencrossies mit Paprikareis',
+        'winner' => [2002, 2004, 2008]],
+    3 => ['name' => 'Spaghetti Bolognese',
+        'winner' => [2011, 2012, 2017]],
+    4 => ['name' => 'Jägerschnitzel mit Pommes',
+        'winner' => [2019]
+]];
+
+function jahreErmitteln($famousMeals)
+{
+    $jahre3 = array();
+        foreach ($famousMeals as $key => $value1) {
+            $jahre1 = implode (",", $value1['winner']);
+            //$jahre2 = explode (",",$jahre1);
+            $jahre3 = array_merge($jahre3,$value1['winner']);
+        }
+        for($i=2000; $i<2021; $i++){
+        if(in_array($i,$jahre3)==false){
+            echo $i.",";
+        }
+}
+}
+?>
+
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Array ausgeben</title>
+</head>
+<body>
+<h3>Hier wird das Array wie gewünscht ausgegeben</h3>
+
+<?php
+    /**foreach ($famousMeals as $key => $value){
+        echo $key, "  ", $value['name'], "<br> \n";
+        foreach($famousMeals as $winner=>$value){
+            echo $value['winner'][$key];}
+        }
+*/
+    foreach ($famousMeals as $key =>$value1){
+        echo $key.".".'&nbsp'.'&nbsp'.'&nbsp'. $value1['name'], "<br> \n";
+            echo '&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'. implode (",  ", $value1['winner']), "<br> \n";
+    }
+?>
+
+<h4>In folgenden Jahren gab es keine Gewinner:</h4>
+<?php echo jahreErmitteln($famousMeals); ?>
+</body>
+</html>
---
+++ beispiele/m2_4e_accesslog.php
@@ -0,0 +1,51 @@
+<?php
+/**
+-Praktikum DBWT. Autoren:
+-Marcel, Buschmann, 3220186
+-Michael Buschmann, 3220197
+ */
+
+//Um den verwendeten Browser aus User Agent raus zu angeln.
+$arr_browsers = ["Opera", "Edg", "Chrome", "Safari", "Firefox", "MSIE", "Trident"];
+
+$agent = $_SERVER['HTTP_USER_AGENT'];
+
+$user_browser = '';
+foreach ($arr_browsers as $browser) {
+    if (strpos($agent, $browser) !== false) {
+        $user_browser = $browser;
+        break;
+    }
+}
+
+switch ($user_browser) {
+    case 'MSIE':
+        $user_browser = 'Internet Explorer';
+        break;
+
+    case 'Trident':
+        $user_browser = 'Internet Explorer';
+        break;
+
+    case 'Edg':
+        $user_browser = 'Microsoft Edge';
+        break;
+}
+
+
+$leerzeichen = "\t";
+$neueZeile= "\n";
+    $server = "IP Adresse: ".$_SERVER['SERVER_ADDR'];
+    $datum = date(DATE_RFC2822);
+    echo $_SERVER['SERVER_ADDR'];
+    $file = fopen('./accesslog.txt','a');
+    fwrite ($file,$server); fwrite($file,$leerzeichen);
+    fwrite($file,$datum); fwrite($file,$leerzeichen);
+    fwrite($file,$user_browser);
+    fwrite ($file,$neueZeile);
+    fclose($file);
+    echo date(DATE_RFC2822);
+
+    echo $user_browser;
+
+?>
